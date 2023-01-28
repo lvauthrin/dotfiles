4ac4488acd35791ce14761442997cce3fd352229
Laurent Vauthrin <lvauthrin@gmail.com>
2023-01-27 23:14:43 -0500

    Added nvim files; added fzf alias

---
+++ config/nvim/lua/plugins/flutter.lua
@@ -0,0 +1,22 @@
+return {
+  -- Dart syntax highlighting
+  {
+    "dart-lang/dart-vim-plugin",
+    config = true,
+  },
+  -- Flutter commands like hot-reload-on-save
+  {
+    "thosakwe/vim-flutter",
+    config = true,
+  },
+  -- lsc for vim
+  {
+    "natebosch/vim-lsc",
+    config = true,
+  },
+  -- lsc for dart
+  {
+    "natebosch/vim-lsc-dart",
+    config = true,
+  },
+}
--- oh-my-zsh.local/aliases.zsh
+++ oh-my-zsh.local/aliases.zsh
@@ -1,1 +1,5 @@
+# Use bat instead of cat
 alias cat='bat'
+
+# Use bat for file previews using fzf
+alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
