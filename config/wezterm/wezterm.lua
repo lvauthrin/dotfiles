3d0b975476768d46f6d12e4228da8a85568f96e3
Laurent Vauthrin <lvauthrin@gmail.com>
2023-03-28 09:16:26 -0400

    Added wezterm config for blur/alpha and workspace configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local act = wezterm.action
 
 local config = {
@@ -7,7 +8,8 @@ local config = {
 	font = wezterm.font_with_fallback({
 		"SauceCodePro Nerd Font Mono",
 	}),
-
+	window_background_opacity = 0.95,
+	macos_window_background_blur = 20,
 	keys = {
 		{
 			key = "h",
@@ -31,5 +33,65 @@ local config = {
 		},
 	},
 }
+
+wezterm.on("gui-startup", function(cmd)
+	-- allow `wezterm start -- something` to affect what we spawn
+	-- in our initial window
+	local args = {}
+	if cmd then
+		args = cmd.args
+	end
+
+	-- Set a workspace for coding on a current project
+	-- Top pane is for the editor, bottom pane is for the build tool
+	local dotfiles_dir = wezterm.home_dir .. "/code/.dotfiles"
+
+	local tab, build_pane, window = mux.spawn_window({
+		workspace = "coding",
+		cwd = dotfiles_dir,
+		args = args,
+	})
+
+	local editor_pane = build_pane:split({
+		direction = "Top",
+		size = 0.75,
+		cwd = dotfiles_dir,
+	})
+
+	-- may as well kick off a build in that pane
+	editor_pane:send_text("z .dotfiles\n")
+	editor_pane:send_text("nvim .\n")
+	build_pane:send_text("z .dotfiles\n")
+
+	-- A workspace for interacting with a local machine that
+	-- runs some docker containners for home automation
+	--local tab, pane, window = mux.spawn_window({
+	--	workspace = "leetcode",
+	--	args = { "z", "leetcode" },
+	--})
+
+	leetcode_dir = wezterm.home_dir .. "/code/leetcode"
+
+	local leet_tab, leet_build_pane = window:spawn_tab({
+		cwd = leetcode_dir,
+	})
+
+	--local leet_tab, leet_build_pane, leet_window = mux.spawn_window({
+	--	workspace = "leetcode",
+	--	cwd = leetcode_dir,
+	--	args = args,
+	--})
+
+	local leet_editor_pane = leet_build_pane:split({
+		direction = "Top",
+		size = 0.75,
+		cwd = leetcode_dir,
+	})
+
+	leet_editor_pane:send_text("z leetcode\n")
+	leet_editor_pane:send_text("nvim .\n")
+	-- We want to startup in the coding workspace
+	mux.set_active_workspace("coding")
+end)
 
 return config
